26c956cd38522745ef38edf7850083b39cbd37dc
unknown <matheusnborgesdev@gmail.com>
2023-12-22 16:09:42 -0300

    api

--- models/Unidade.js
+++ models/Unidade.js
@@ -3,7 +3,8 @@ const connection = require('../database/connection')
 class Unidade{
 
     async getUnidades(cidade){
-        const[rows] = await connection.query(`select * from unidade where fk_cidade = ${cidade}`)
+        const[rows] = await connection.query(`select unidade.nome, unidade.endereco, unidade.bairro, unidade.telefone, tipo_unidade.nome as tipo from unidade JOIN tipo_unidade ON unidade.fk_tipo = tipo_unidade.pk_tipo where fk_cidade = ${cidade}`)
+
         return rows
     }
 }
