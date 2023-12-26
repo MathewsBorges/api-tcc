7bf0e7fe936daf1d2fd49be5371fd9e836f95430
unknown <matheusnborgesdev@gmail.com>
2023-12-26 16:04:39 -0300

    att medico

--- models/Medico.js
+++ models/Medico.js
@@ -1,6 +1,5 @@
 const connection = require("../database/connection");
 
-
 class Medico {
   async getMedicos() {
     const [rows] = await connection.query(
@@ -9,8 +8,7 @@ class Medico {
     return rows;
   }
 
-
-  //Pega o dia e converte em dia da semana 
+  //Pega o dia e converte em dia da semana
   async getHorariosByDia(id, dia) {
     const [rows] = await connection.query(
       `SELECT * from horarios where fk_medico = '${id}' and dia_semana = '${dia}'`
@@ -18,31 +16,12 @@ class Medico {
     return rows;
   }
 
-  async getHorarios(id) {
-    const [rows] = await connection.query(
-      `SELECT * from horarios where fk_medico = '${id}'`
-    );
-    return rows;
-  }
-
-
-
- 
-
-
-
-
-
-
-
-
   async getMedicoByUnidade(unidade) {
     const [rows] = await connection.query(
-    `SELECT medico.*
-    FROM medico
-    INNER JOIN horarios ON medico.pk_medico = horarios.fk_medico
-    WHERE horarios.fk_unidade = ?`,
-      unidade
+      `SELECT DISTINCT medico.*, especialidade.nome AS fk_especialidade
+       FROM medico INNER JOIN horarios ON medico.pk_medico = horarios.fk_medico
+       INNER JOIN especialidade ON medico.fk_especialidade = especialidade.pk_especialidade
+       WHERE horarios.fk_unidade = ${unidade} and medico.status = 1`
     );
     return rows;
   }
